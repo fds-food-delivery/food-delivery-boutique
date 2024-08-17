ae99b969e95572df29132e497db22848a07d5b34
Souleymane KODJO <76671691+kodjoSoule@users.noreply.github.com>
2024-08-17 13:27:02 +0000

    refractor navabr and design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className=' custom-header header mt-5'>
         <div className="header-contents">
             <h2>Commandez ici votre plat préféré</h2>
             <p>Choisissez parmi un menu varié proposant une gamme délicieuse de plats préparés avec les meilleurs ingrédients et l'expertise culinaire. Notre mission est de satisfaire vos envies et d'améliorer votre expérience culinaire, un délicieux repas à la fois.</p>
--- src/components/Navbar/Navbar3.jsx
+++ src/components/Navbar/Navbar3.jsx
@@ -1,14 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Navbar, Container, Nav, NavDropdown, Badge } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
+import { StoreContext } from '../../context/StoreContext';
+import Panier from '../../pages/Panier/Panier';
+import CartIconWithCount from "./CartIconWithCount.jsx";
+
 function NavigationBar() {
+    const [isPanierOpen, setIsPanierOpen] = useState(false);
+    const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+    const { cartItems, currentUser, logout } = useContext(StoreContext);
+    const navigate = useNavigate();
+
+    const totalItems = cartItems
+        ? Object.values(cartItems).reduce((sum, quantity) => sum + quantity, 0)
+        : 0;
+
+    const togglePanier = () => {
+        setIsPanierOpen(!isPanierOpen);
+    };
+
+    const handleProfileClick = () => {
+        navigate('/profil'); // Naviguer vers la page de profil
+        setIsUserMenuOpen(false); // Fermer le menu après avoir cliqué sur le profil
+    };
+
+    const handleLogout = async () => {
+        await logout();
+        navigate("/login");
+    };
+
     useEffect(() => {
         const handleScroll = () => {
             const navbar = document.querySelector('.navbar');
             if (window.scrollY > 50) {
-                navbar.classList.add('bg-black', 'navbar-sticky', 'shadow-sm');
+                navbar.classList.add('', 'navbar-sticky', 'shadow-sm');
             } else {
-                navbar.classList.remove('bg-black', 'navbar-sticky', 'shadow-sm');
+                navbar.classList.remove('', 'navbar-sticky', 'shadow-sm');
             }
         };
 
@@ -20,36 +48,48 @@ function NavigationBar() {
     }, []);
 
     return (
-        <Navbar className="no-tailwind custom-navbar navbar-expand-lg fixed-top navbar-light" expand="md">
+        <Navbar className="custom-navbar pt-3 navbar-expand-lg fixed-top navbar-light" expand="md">
             <Container>
                 <Navbar.Brand as={Link} to="/">
                     <img
-                        src="https://example.com/logo.png" // Remplacez par l'URL de votre logo
+                        src="/images/logo.png"
                         alt="Thurquoise Logo"
                         className="d-inline-block align-top"
-                        width="40"
+                        width="70"
                     />
-                    <span className="brand-name">Thurquoise</span>
+                    {/*<span className="brand-name">Thurquoise</span>*/}
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="custom-basic-navbar-nav basic-navbar-nav"  />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/" className="custom-nav-link">Accueil</Nav.Link>
                         <Nav.Link as={Link} to="/menu" className="custom-nav-link">Menu</Nav.Link>
-                        <Nav.Link as={Link} to="/about" className="custom-nav-link">About Us</Nav.Link>
+                        <Nav.Link as={Link} to="/livraison" className="custom-nav-link">Livraison</Nav.Link>
                         <Nav.Link as={Link} to="/contact" className="custom-nav-link">Contact</Nav.Link>
-                        <Nav.Link as={Link} to="/cart" className="custom-nav-link">
-                            Cart <Badge pill bg="danger">3</Badge> {/* Exemple : Nombre d'articles */}
+                        <Nav.Link as={Link} to="/cart" className="custom-nav-link" onClick={togglePanier}>
+                            <div className="flex items-center">
+                                <CartIconWithCount size={28} itemCount={totalItems}/>
+                            </div>
+                            {/*Panier <Badge pill bg="danger">{totalItems}</Badge>*/}
                         </Nav.Link>
-                        <NavDropdown title="Account" id="basic-nav-dropdown" className="custom-nav-link">
-                            <NavDropdown.Item as={Link} to="/login">Login</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/register">Register</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item as={Link} to="/profile">Profile</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/orders">My Orders</NavDropdown.Item>
-                        </NavDropdown>
+                        {currentUser ? (
+                            <NavDropdown title={<FaUser size={28}  />} id="basic-nav-dropdown" className="custom-nav-link ">
+                                {/*show username*/}
+                                <NavDropdown.Item className="text-uppercase
+                                text-gray-800
+                                ">{currentUser.username || 'Utilisateur'}</NavDropdown.Item>
+                                {/*bar */}
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item onClick={handleProfileClick}>Profil</NavDropdown.Item>
+                                <NavDropdown.Item onClick={handleLogout}>Déconnexion</NavDropdown.Item>
+                            </NavDropdown>
+                        ) : (
+                            <Nav.Link as={Link} to="/login" className="custom-nav-link">Login</Nav.Link>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
+            {isPanierOpen && <Panier onClose={togglePanier} />}
         </Navbar>
     );
 }
