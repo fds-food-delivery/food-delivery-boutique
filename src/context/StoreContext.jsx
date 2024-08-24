7ca306691a4fbeefe4862067d0b12e867f9056d9
Souleymane KODJO <76671691+kodjoSoule@users.noreply.github.com>
2024-08-24 14:29:18 +0000

    update delivered page

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -16,7 +16,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 	const [cartItems, setCartItems] = useState({});
 
-	 const url = "https://backend-food-ordering.onrender.com";
+	const url = "https://backend-food-ordering.onrender.com";
 	//const url = "http://localhost:4000";
 	const [foodList, setFoodList] = useState([]);
 	const [loading, setLoading] = useState(false);
