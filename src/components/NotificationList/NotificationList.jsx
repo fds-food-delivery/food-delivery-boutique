3f3b23a6499115ffb6f94c0f8f97653cf3d6c9f8
Souleymane KODJO <76671691+kodjoSoule@users.noreply.github.com>
2024-09-04 22:57:38 +0000

    feat : add feat notification

--- src/components/NotificationList/NotificationList.jsx
+++ src/components/NotificationList/NotificationList.jsx
@@ -5,6 +5,11 @@ import {Link} from "react-router-dom";
 
 const NotificationList = ({ notifications, onClose }) => {
     notifications = notifications.slice(0, 3); // Display only the first 3 notifications
+    function formatDate(dateString) {
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+        const date = new Date(dateString);
+        return date.toLocaleDateString('fr-FR', options);
+    }
     return (
         <div className="notification-list-container">
             <ul className="notification-list">
@@ -12,7 +17,9 @@ const NotificationList = ({ notifications, onClose }) => {
                     notifications.map((notification, index) => (
                         <li key={index} className="notification-item">
                             <span>{notification.message}</span>
-                            <small className="text-muted d-block">{notification.date}</small>
+                            <small className="text-muted d-block">{
+                                formatDate(notification.date)
+                            }</small>
                         </li>
                     ))
                 ) : (
